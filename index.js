910072915d97857a12c7ceb78ce4d49ba490442f
vladimir <logovazzik@gmail.com>
2017-11-11 19:24:35 +0300

    add bityou

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
     function RateService(){
+        var proxy = 'https://otn.org/assets/resources/proxy.php?url=';
+
         this._getBTCrate =  function(){
             return $.ajax({
                 crossDomain: true,
                 dataType: "json",
-                url: "https://blockchain.info/tobtc?currency=USD&value=1"
+                url: proxy + decodeURIComponent('https://iqoption.com/api/candles/history?active_id=816')
             }).then(function(data){
-              return data;
+                return data.result.actives[0].rate;
             })
         };
        this.getCRtopiaPrice =  function() {
             return this._getBTCrate().then(function (rate) {
                 return $.ajax({
-                    url: "https://www.cryptopia.co.nz/api/GetMarket/OTN_BTC",
+                    url: proxy + decodeURIComponent("https://www.cryptopia.co.nz/api/GetMarket/OTN_BTC"),
                     type: "GET",
                     crossDomain: true,
                     dataType: "json"
                 }).then(function (data) {
                     var _data;
                     if((_data = (data && data.Data && data.Data.LastPrice))){
-                        debugger
-                        return (_data * (1/rate)).toFixed(2)
-                    };
-
+                        return (_data * rate).toFixed(2)
+                    }
                 })
             });
         }
         this.getLiveCoinPrice =  function(){
             return this._getBTCrate().then(function (rate ) {
                 return $.ajax({
-                    url: "https://otn.org/assets/resources/proxy.php?url=" + decodeURIComponent("https://api.livecoin.net/exchange/ticker?currencyPair=OTN/BTC&callback=?"),
+                    url: proxy + decodeURIComponent("https://api.livecoin.net/exchange/ticker?currencyPair=OTN/BTC&callback=?"),
                     type: "GET",
                     crossDomain: true
                 }).then(function(data){
                     var bestAsk;
                     if((bestAsk = data && data.best_ask)){
-                        return (bestAsk * (1/rate)).toFixed(2)
-                    };
+                        return (bestAsk * rate).toFixed(2)
+                    }
+
+                })
+            });
+        }
+
+        this.getYouBitCoinPrice =  function(){
+            return this._getBTCrate().then(function (rate ) {
+                return $.ajax({
+                    url: proxy + decodeURIComponent("https://yobit.net/api/2/otn_btc/ticker"),
+                    type: "GET",
+                    crossDomain: true
+                }).then(function(data){
+                    return (rate * data.ticker.last).toFixed(2);
 
                 })
             });
@@ -47,7 +60,8 @@
     function MainView(){
         this.init = function(){
             var $cryptoValue = $('.rate__value_crypto'),
-                $liveValue = $('.rate__value_livecoin');
+                $liveValue = $('.rate__value_livecoin'),
+                $youbitValue = $('.rate__value_youbit');
             var rateService = new RateService();
 
             rateService.getLiveCoinPrice().then(function(value){
@@ -55,6 +69,9 @@
             })
             rateService.getCRtopiaPrice().then(function(value){
                 $liveValue.html('$' + value);
+            })
+            rateService.getYouBitCoinPrice().then(function(value){
+                $youbitValue.html('$' + value);
             })
 
         };
